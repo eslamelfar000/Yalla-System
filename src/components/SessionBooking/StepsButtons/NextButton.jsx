b599f19919a8baf940960fa026f9b6d63e361990
eslamelfar000 <eslamsaber707@gmail.com>
2025-03-30 03:24:00 +0200

    last

--- src/components/SessionBooking/StepsButtons/NextButton.jsx
+++ src/components/SessionBooking/StepsButtons/NextButton.jsx
@@ -13,7 +13,7 @@ function NextButton({ selected,block }) {
       <button
         disabled={block}
         className={`btn text-white font-[600] bg-main py-2 px-5 rounded-xl shadow-none border-2 ${
-          booking?.eventDate?.length !== 0 && "border-main"
+          booking?.eventDate?.length !== 0 || currentStep === "bookingType"&& "border-main hover:border-main-dark"
         }  border-solid hover:bg-main-dark  transition-all duration-300 `}
         onClick={() => {
           dispatch(setStep("sessionCalendar"));
