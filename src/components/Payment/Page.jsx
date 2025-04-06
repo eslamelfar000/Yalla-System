e8e26560d13450fa806c221b0ff4609f4330c492
eslamelfar000 <eslamsaber707@gmail.com>
2025-04-07 00:34:04 +0200

    last

--- src/components/Payment/Page.jsx
+++ src/components/Payment/Page.jsx
@@ -13,8 +13,8 @@ function Page() {
 
   return (
     <>
-      <div className="cover flex justify-center items-start min-h-[calc(100vh-10rem)]">
-        <div className="cover lg:flex justify-center items-start  gap-5 py-15 w-[95%] md:w-[80%] lg:w-[90%] 2xl:w-[70%]">
+      <div className="cover flex justify-center items-start lg:min-h-[calc(100vh-10rem)]">
+        <div className="cover lg:flex justify-center items-start  gap-5 py-5 lg:py-15 w-[92%] md:w-[80%] lg:w-[90%] 2xl:w-[70%]">
           <PaymentMethods handleSelect={handleSelect} selected={selected} />
           <PaymentDetails selected={selected} />
         </div>
