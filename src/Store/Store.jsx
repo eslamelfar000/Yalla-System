e9c39de095e297c97db8cf8979eb979ce26a1dda
eslamelfar000 <eslamsaber707@gmail.com>
2025-04-15 15:20:55 +0200

    last

--- src/Store/Store.jsx
+++ src/Store/Store.jsx
@@ -1,7 +1,7 @@
 import { configureStore } from "@reduxjs/toolkit";
 import stepReducer from "./Reducer/stepSlice";
 import bookingReducer from "./Reducer/bookingSlice";
-import notificationReducer from "./Reducer/NotificationSlice";
+import notificationReducer from "./Reducer/notificationSlice";
 
 export const store = configureStore({
   reducer: {
